8e7418eda0fb4a13fefc2e1840e58a6ae3241d63
StevanFreeborn <sfreeborn7@gmail.com>
2022-07-16 18:49:05 -0500

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+
+            return View(movie);
+        }
+    }
+}
