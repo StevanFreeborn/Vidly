dc18d61c12afa608186bf438394c4955e5ca9d11
StevanFreeborn <sfreeborn7@gmail.com>
2022-07-16 19:02:52 -0500

    added index action to movies controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -18,6 +14,16 @@ namespace Vidly.Controllers
             };
 
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id={id}");
+        }
+
+        public ActionResult Index(int pageIndex = 1, string sortBy = "Name")
+        {
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
         }
     }
 }
