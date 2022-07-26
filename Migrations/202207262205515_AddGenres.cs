5982801d2d5d74aad3be9b06aac2bc5bcc4db87d
StevanFreeborn <sfreeborn7@gmail.com>
2022-07-26 17:15:49 -0500

    get movies from database and display movie genre on movies index view

---
+++ Migrations/202207262205515_AddGenres.cs
@@ -0,0 +1,22 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class AddGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Drama')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Comedy')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Adventure')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (6, 'Fantasy')");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
