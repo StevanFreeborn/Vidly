468a24f4e97c21e3df7483eb9df01c1ed2f3a621
StevanFreeborn <sfreeborn7@gmail.com>
2022-08-02 10:26:20 -0500

    implemented getting customers and rendering customers table on the client instead of on the server

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -22,16 +22,7 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var customers = _context.Customers
-                .Include(c => c.MembershipType)
-                .ToList();
-
-            var viewModel = new CustomersViewModel
-            {
-                Customers = customers
-            };
-
-            return View(viewModel);
+            return View();
         }
 
         public ActionResult Add()
