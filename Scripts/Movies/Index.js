81b6b0833fb4e83abcd0e94844213b0ad474b527
StevanFreeborn <sfreeborn7@gmail.com>
2022-08-05 09:55:22 -0500

    moved javascript out of index views and into separate js files

---
+++ Scripts/Movies/Index.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(() => {
+    $("#movies-table").DataTable({
+        ajax: {
+            url: "/api/movies",
+            dataSrc: ""
+        },
+        columns: [
+            {
+                data: "name",
+                render: (data, type, movie) => {
+                    return `<a href="/movies/edit/${movie.id}">${movie.name}</a>`;
+                }
+            },
+            {
+                data: "genre.name"
+            }
+        ]
+    });
+});
