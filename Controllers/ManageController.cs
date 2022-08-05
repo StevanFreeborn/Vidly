c8f6c5b098e80d3f4c6a669e9693183af623b0b3
StevanFreeborn <sfreeborn7@gmail.com>
2022-08-05 17:33:57 -0500

    refactored identity models in to separate fields, refactored account view models into separate files and moved under view models namespace, refactored manage view models into separate files, and moved under view model namespace, and updated manage and account controllers accordingly.

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -5,7 +5,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
