f171b27953340170d00abdb2e7050c30921d1e03
StevanFreeborn <sfreeborn7@gmail.com>
2022-08-09 11:41:14 -0500

    add ability to remove movies from selected list

--- Scripts/Views/Rentals/RentalForm.js
+++ Scripts/Views/Rentals/RentalForm.js
@@ -14,6 +14,16 @@
         }
     });
 
+    const movies = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/api/movies?query=%QUERY',
+            wildcard: '%QUERY',
+            cache: false,
+        }
+    });
+
     $('#customer').typeahead({
         minLength: 3,
         highlight: true
@@ -25,16 +35,6 @@
         viewModel.customerId = customer.id;
     });
 
-    const movies = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        remote: {
-            url: '/api/movies?query=%QUERY',
-            wildcard: '%QUERY',
-            cache: false,
-        }
-    });
-
     $('#movie').typeahead({
         minLength: 3,
         highlight: true,
@@ -43,15 +43,44 @@
         display: 'name',
         source: movies,
     }).on('typeahead:select', (e, movie) => {
-
         if (viewModel.movieIds.indexOf(movie.id) !== -1) return;
 
         $('#moviesPlaceholder').remove();
-        $("#selectedMovies").append(`<li class="list-group-item">${movie.name}</li>`);
+
+        $("#selectedMovies").append(
+            `<li id="${movie.id}" class="list-group-item d-flex justify-content-between align-items-center">
+                ${movie.name}
+                <span>
+                    <button data-movie-id=${movie.id} class="btn btn-danger btn-delete">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                </span>
+             </li>`
+        );
+
         $('#movie').typeahead('val', '');
+
         viewModel.movieIds.push(movie.id);
     });
 
     // allow form fields to span width of container.
     $('.twitter-typeahead').attr('style', 'position: relative, display: block');
+
+    $("#selectedMovies").on('click', '.btn-delete', (e) => {
+        const button = $(e.currentTarget);
+
+        const movieId = parseInt(button.attr('data-movie-id'));
+
+        const movieIds = viewModel.movieIds;
+
+        const index = movieIds.indexOf(movieId);
+
+        if (index === -1) return;
+
+        movieIds.splice(index, 1);
+
+        button.parents('li').remove();
+
+        if (movieIds.length === 0) $('#selectedMovies').append('<li id="moviesPlaceholder" class="list-group-item">No movies selected.</li>');
+    });
 })
