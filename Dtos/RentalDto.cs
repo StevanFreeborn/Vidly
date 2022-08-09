306164495e4f1c9320d21ae8fcaa95aa2598938c
StevanFreeborn <sfreeborn7@gmail.com>
2022-08-08 23:58:11 -0500

    implemented backend of rentals

---
+++ Dtos/RentalDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class RentalDto
+    {
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public List<int> MovieIds { get; set; }
+    }
+}
